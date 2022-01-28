d1e0e9dcf9e30c9f91e4c9c7114a0364687dd99a
Amit <amin>
2022-01-28 16:15:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+      photos:[],
+      photoLink:[]
+  }
+
+
+    getPhotos = () => {
+        axios.get("https://api.flickr.com/services/rest/", {
+            params: {
+                method: 'flickr.photos.getRecent',
+                extras: 'url_s',
+                format:'json' ,
+                nojsoncallback:'1',
+                api_key: 'aabca25d8cd75f676d3a74a72dcebf21'
+            }}).then((response=>{
+            console.log(response.data.photos);
+                this.setState({
+                    photos:response.data.photos,
+                })
+
+        }))
+    }
+
+  render() {
+    return (
+        <div>
+<h1>Gallery</h1>
+
+            {console.log(this.state.photos)}
+            <button onClick={this.getPhotos}>click</button>
+        </div>
+    );
+  }
 }
-
 export default App;
